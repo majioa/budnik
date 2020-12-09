738e0f06e855fe446b2ebdc9b184ddd758d25327
Malo Skrylevo <majioa@yandex.ru>
2020-12-09 19:49:03 +0300

    fix: config
    ! config lines, and new data in it

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,10 @@
 use Mix.Config
 
 config :nadia,
-  token: {:system, "TELEGRAM_APP_TOKEN"}
-  recv_timeout: 10
+   token: {:system, "TELEGRAM_APP_TOKEN"},
+   recv_timeout: 10
+
+config :logger, :console,
+   level: :info,
+   format: "$date $time [$level] $metadata$message\n",
+   metadata: [:user_id]
